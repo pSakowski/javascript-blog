d3adcbd05c95e51a2cddc01cf090e8a96ab5fdb0
psakowski <p.sakowski1213@gmail.com>
2022-08-25 13:23:48 +0200

    fix script.js

--- js/script.js
+++ js/script.js
@@ -36,7 +36,8 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
-  optArticleAuthorSelector = '.post-author';
+  optArticleAuthorSelector = '.post-author',
+  optTagsListSelector = '.tags.list';
 
 
 const generateTitleLinks = function(customSelector = ''){
@@ -71,6 +72,8 @@ generateTitleLinks();
 
 /* 7.2 */
 const generateTags = function(){
+  /* [NEW] create a new variable allTags with an empty array */
+  let allTags = [];
   /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
   /* START LOOP: for every article: */
@@ -88,14 +91,24 @@ const generateTags = function(){
     for(let tag of articleTagsArray){
       //console.log(tag);
       /* generate HTML of the link */
-      const linkHTML = `<li><a href="#tag-${tag}">${tag}</a></li>`;
+      const linkHTML = '<li><a href="#tag-' + tag + '"><span>' + tag + '</span></a></li>';
       /* add generated code to html variable */
       html = html + linkHTML;
+      /* [NEW] check if this link is NOT already in allTags */
+      if(allTags.indexOf(linkHTML) == -1){
+        /* [NEW] add generated code to allTags array */
+        allTags.push(linkHTML);
+        console.log(allTags);
+      }
     }
     /* END LOOP: for each tag */
     /* insert HTML of all the links into the tags wrapper */
     titleList.innerHTML = html;
     /* END LOOP: for every article: */
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector(optTagsListSelector);
+    /* [NEW] add html from allTags to tagList */
+    tagList.innerHTML = allTags.join('');
   }
 };
 
